9b4e995790d2b99590f97d1ff2e9246f845fa7ad
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2019-02-05 11:57:45 -0300

    Se agrega two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'directorio comercial angular';
   deshabilitado = true;
+  name = '';
 
   constructor() {
     setTimeout(() => {
